df4aaac6c043d8aee3e10cd539205c50f5cebf12
chiaanna <wangchia6688@gmail.com>
2020-06-13 21:39:00 +0800

    json test

--- src/main/java/com/yarui/Read/GsonExcelTransfer.java
+++ src/main/java/com/yarui/Read/GsonExcelTransfer.java
@@ -2,22 +2,31 @@ package com.yarui.Read;
 
 import java.io.FileWriter;
 import java.io.IOException;
-
 import com.google.gson.Gson;
+
+import net.sf.json.JSONArray;
 
 public class GsonExcelTransfer {
 	public static void main(String[] args) {
 		Gson gson = new Gson();
-        Staff staff = ImportExcelMain();
-        // Java objects to String
-        // String json = gson.toJson(staff);
+		boolean readSheetNum = false;
+		int readSheet = 1;
+		ImportExeclMain_byMan poi = new ImportExeclMain_byMan();
 
-        // Java objects to File
-        try (FileWriter writer = new FileWriter("C:\\projects\\staff.json")) {
-            gson.toJson(staff, writer);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-		
+		JSONArray list = poi.readExcel("C:\\Users\\asus\\Desktop\\港務測站資料.xlsx");
+		Gson gjson = new Gson();
+		System.out.println(list.size());
+		// Java objects to String
+		// String json = gson.toJson(staff);
+
+		// Java objects to File
+		try {
+			FileWriter writer = new FileWriter("C:\\projects\\staff2.json");
+			writer.write(list.toString());
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
